2c6cee1909180bd33951e849fba3691c6adf02bc
Mingi Kim <loopang5954@gmail.com>
2026-09-14 16:23:18 +0900

    feat: 일관된 폼 입력 UI를 위한 공통 Input과 Label 추가(#4) (#7)
    * feat: 일관된 폼 입력 UI를 위한 공통 Input과 Label 추가(#4)
    
    * docs: 커밋 메시지 생성 규칙 문서화(#4)
    
    * fix: Input 테두리 색상 충돌을 제거하고 포커스 시 오류 색상 유지

---
+++ src/components/ui/form/input/Input.tsx
@@ -0,0 +1,39 @@
+/**프로젝트 전반에서 사용하는 공통 input 컴포넌트입니다. */
+/**label 컴포넌트랑 같이 사용하면 됩니다. */
+
+import React, { forwardRef } from "react";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isError?: boolean;
+}
+
+//현재 w-full을 사용해서 가로 길이 전체를 사용하는데 나중에 길이 조절이 필요하면 props 방식으로 수정
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ isError, className = "", disabled, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        disabled={disabled}
+        className={`
+          w-full h-12 
+          px-5 py-3.5
+          bg-white text-gray-800 text-base
+          placeholder:text-gray-500
+          rounded-2xl
+          border border-gray-300 transition-colors duration-200 outline-none
+          ${
+            isError
+              ? "border-error-100"
+              : "focus:border-primary-500"
+          }
+          disabled:bg-gray-50 disabled:text-gray-400 disabled:cursor-not-allowed
+          ${className}
+        `}
+        {...props}
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
+export default Input;
