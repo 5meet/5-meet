c891f6fbf7aa62b3023fe86cfdc578ef2198a234
chaewon127 <osobi621@gmail.com>
2026-09-14 17:53:04 +0900

    feat:ProgressBar 기본 컴포넌트 구현

---
+++ src/components/ui/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+interface ProgressBarProps {
+  percentage: number;
+}
+
+const ProgressBar = ({ percentage }: ProgressBarProps) => {
+  return (
+    <div className="h-2 w-full">
+      <div className="h-full w-full rounded-[16px] border border-none bg-gray-200">
+        <div
+          className="h-full rounded-[16px] border border-none bg-mint-gradient-500"
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
