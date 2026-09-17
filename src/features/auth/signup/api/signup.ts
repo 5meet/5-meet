7050810d89fdaf5766ec81e3ceb65e5f26a06944
carrbanog <loopang5954@gmail.com>
2026-09-17 14:46:49 +0900

    refactor: API 오류 처리를 Ky beforeError 훅으로 통합 (#25)

--- src/features/auth/signup/api/signup.ts
+++ src/features/auth/signup/api/signup.ts
@@ -1,19 +1,11 @@
 import { api } from "@/lib/api/api";
-import { getApiError } from "@/lib/api/getApiError";
 import type { SignupRequest, SignupResponse } from "../types";
 
+// signup 함수에서는 순수하게 통신만하고 에러처리 X
 export const signup = async (data: SignupRequest): Promise<SignupResponse> => {
-  try {
-    const response = await api
-      .post("auth/signup", {
-        json: data,
-      })
-      .json<SignupResponse>();
-    console.log("회원가입 성공 응답:", response);
-    return response;
-  } catch (error) {
-    const apiError = getApiError(error);
-    console.log("signup.ts 에러", apiError);
-    throw apiError;
-  }
+  return await api
+    .post("/auth/signup", {
+      json: data,
+    })
+    .json<SignupResponse>();
 };
--- src/lib/api/getApiError.ts
+++ src/lib/api/getApiError.ts
@@ -1,6 +1,8 @@
 import { HTTPError } from "ky";
 import type { ApiErrorResponse } from "./type";
 
+// 에러 헨들링이 아닌 마무리로 에러를 객체로 변환해서 modal에 띄우거나 화면에 보여주는 용도로 사용
+// 이 코드는 현재 사용하지 않습니다. ky 요청 에러 처리는 api.ts파일에서 담당하고 추후에 서버 컴포넌트 에러 처리를 추가하면서 사용할 예정입니다.
 export const getApiError = (error: unknown): ApiErrorResponse => {
   // 1. ky의 HTTP 요청 에러
   // 서버가 4xx, 5xx 응답을 반환했을 때
