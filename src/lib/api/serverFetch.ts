85a505d1e208cf20a16bfdc922a29e06629e19f0
carrbanog <loopang5954@gmail.com>
2026-09-19 19:38:10 +0900

    fix: 서버 API 요청의 타임아웃과 외부 취소 신호를 함께 처리 (#27)

--- src/lib/api/serverFetch.ts
+++ src/lib/api/serverFetch.ts
@@ -1,5 +1,5 @@
 import { ApiErrorResponse } from "./type";
-import { ApiError } from "./ApiError"
+import { ApiError } from "./ApiError";
 
 interface FetchOptions extends RequestInit {
   timeout?: number; // 기본 타임아웃 지원 (ms)
@@ -9,37 +9,60 @@ export async function serverFetch<T>(
   url: string,
   options: FetchOptions = {},
 ): Promise<T> {
-  const { timeout = 10000, ...fetchOptions } = options;
+  const {
+    timeout = 10000,
+    signal: externalSignal, //외부에서 전달해준 시그널
+    ...fetchOptions
+  } = options;
+
+  // 기본 timeout용 signal
+  const timeoutSignal = AbortSignal.timeout(timeout);
+
+  const signal = externalSignal
+    ? AbortSignal.any([externalSignal, timeoutSignal])
+    : timeoutSignal;
 
   try {
     const response = await fetch(url, {
       ...fetchOptions,
-      // AbortSignal.timeout으로 타임아웃 자동 제어
-      signal: options.signal ?? AbortSignal.timeout(timeout),
+      signal,
     });
 
     // 1. 서버 HTTP 에러 (4xx, 5xx)
     if (!response.ok) {
       const errorData = (await response.json()) as ApiErrorResponse;
 
-      throw new ApiError(errorData.message, errorData.code, response.status);
+      throw new ApiError(
+        errorData.message,
+        errorData.code,
+        response.status,
+      );
     }
 
     return (await response.json()) as T;
   } catch (error) {
-    // 2. !response.ok에서 생성한 apiError 처리
+    // 2. !response.ok에서 생성한 ApiError
     if (error instanceof ApiError) {
       throw error;
     }
-    // 3. 타입 아웃
+
+    // 3. 타임아웃
     if (error instanceof Error && error.name === "TimeoutError") {
       throw new ApiError(
-        "요청 시간이 초과되었습니다. 다시 시도해주세요",
+        "요청 시간이 초과되었습니다. 다시 시도해주세요.",
         "TIMEOUT_ERROR",
       );
     }
 
-    // 4. 네트워크 에러
+    // 4. 외부 AbortSignal에 의한 요청 취소
+    if (error instanceof Error && error.name === "AbortError") {
+      throw new ApiError(
+        "요청이 취소되었습니다.",
+        "ABORT_ERROR",
+      );
+    }
+
+    // 5. 네트워크 에러
     if (error instanceof TypeError) {
       throw new ApiError(
         "네트워크 연결이 불안정합니다. 잠시 후 다시 시도해주세요.",
@@ -47,7 +70,10 @@ export async function serverFetch<T>(
       );
     }
 
-    // 5. 예상하지 못한 에러
-    throw new ApiError("알 수 없는 오류가 발생했습니다.", "UNKNOWN_ERROR");
+    // 6. 예상하지 못한 에러
+    throw new ApiError(
+      "알 수 없는 오류가 발생했습니다.",
+      "UNKNOWN_ERROR",
+    );
   }
 }
