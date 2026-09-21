6629d08f65da4b6a0a525712796661c209dffc0c
carrbanog <loopang5954@gmail.com>
2026-09-21 16:10:04 +0900

    fix: 루트 레이아웃 충돌 해소 및 전역 토스트 연결 (#27)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import Providers from "./provider";
-=======
 import { AppToaster } from "@/components/ui/Sonner";
->>>>>>> d6fb9085ba90067421eff430735fc8c91b05a361
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,12 +26,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-<<<<<<< HEAD
-        <Providers>{children}</Providers>
-=======
-        {children}
-        <AppToaster />
->>>>>>> d6fb9085ba90067421eff430735fc8c91b05a361
+        <Providers>
+          {children}
+          <AppToaster />
+        </Providers>
       </body>
     </html>
   );
