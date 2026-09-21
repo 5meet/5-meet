6de23a99a0923b92933e33060eb27cb89c268ec6
chaewon127 <osobi621@gmail.com>
2026-09-21 18:00:03 +0900

    feat:반응형 추가

--- src/components/ui/MeetingDetail/ReviewCard/ReviewCardList.tsx
+++ src/components/ui/MeetingDetail/ReviewCard/ReviewCardList.tsx
@@ -8,14 +8,14 @@ interface ReviewCardListProps {
 const ReviewCardList = ({ reviews }: ReviewCardListProps) => {
   if (reviews.length === 0) {
     return (
-      <section className="flex w-[343px] items-center justify-center px-5 pt-4 pb-2 rounded-4xl shadow-md lg:px-12 lg:py-6 md:w-174 lg:w-7xl">
+      <section className="flex w-[343px] items-center justify-center px-5 pt-4 pb-2 rounded-4xl shadow-md md:px-12 md:py-6 md:w-174 lg:px-12 lg:py-6 lg:w-7xl">
         <EmptyReview />
       </section>
     );
   }
 
   return (
-    <section className="flex flex-col w-[343px] px-5 pt-4 pb-2 rounded-4xl shadow-md lg:px-12 lg:py-6 md:w-174 lg:w-7xl">
+    <section className="flex flex-col w-[343px] px-5 pt-4 pb-2 rounded-4xl shadow-md md:px-12 md:py-6 md:w-174 lg:px-12 lg:py-6 lg:w-7xl">
       {reviews.map((review, index) => (
         <ReviewCard
           key={`${review.user.id}-${review.datetime}`}
